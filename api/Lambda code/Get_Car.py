fff3dc565c85b7d3cadbcd5de6b23a10496b501e
unknown <csckhw@gmail.com>
2019-08-14 23:27:15 -0400

    update

--- api/Lambda code/Get_Car.py
+++ api/Lambda code/Get_Car.py
@@ -4,11 +4,12 @@ def get_car(event, context):
     try:
         s3 = boto3.resource('s3')
         client_s3 = boto3.client('s3')
-        brand_upper = event["brand"].upper()
+        brand_upper = event["brand"]
         bucket = s3.Bucket("car-images-chan")
 
         for obj in bucket.objects.filter(Prefix=brand_upper + '/'):
-            url = "https://s3-us-east-1.amazonaws.com/" + obj.bucket_name + "/" + obj.key
+            url = "https://s3-us-east-1.amazonaws.com/" + obj.bucket_name + "/" + obj.key + "car.jpg"
+            
             return ("Image URL is {0}".format(url))
         return("*** Failure to retrieve Car Image - Please check your request ***")
 
